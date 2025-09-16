75d8898f3e5cc18505b042eec1524497cb897f85
Pruthvi Vikram <vikrampruthvi5@gmail.com>
2025-09-16 15:18:05 -0400

    Configured state backend to S3

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 6.0"
     }
   }
+  backend "s3" {
+    bucket         = "sre-learning-project-terraform-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+  }
 }
 
 # Configure the AWS Provider
